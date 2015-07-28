e8b5cd974f4f6bd2af692768aa251775ea88fe8d
Stanley McMillan <stanalanmcm@gmail.com>
2015-07-27 20:26:08 -0400

    added ability to remove items.  Draft 1 is complete.

--- app.js
+++ app.js
@@ -1,38 +1,42 @@
+var input,
+    form = document.querySelector('[data-js~="todoForm"]'),
+    list;
+
 ;(function() {
-  var input,
-      list;
 
-  function stopDef(event) {
-    event.preventDefault()
-    input = document.querySelector('[data-js~="todoInput"]').value;
-    if (input === "") {
-      alert("Input is blank. Vader will force choke you for this!")
-      }
-      else {
-      list = document.querySelector('[data-js~="todoItems"]');
-      if (list === null) {
-        list = document.querySelector('[data-js~="newItems"]');
-        list.innerHTML +=" <li class='todo__item'><input type='checkbox' id='" + input + "' /> <label for='" + input + "'>" + input + "</label><button value='" + input + "' class='todo__itemRemove'>&#x2717;</button></li>"
-      }
-      else {
-        list.innerHTML ="<li class='todo__item'><input type='checkbox' id='" + input + "' /> <label for='" + input + "'>" + input + "</label><button value='" + input + "' class='todo__itemRemove'>&#x2717;</button></li>"
-        list.dataset.js = "newItems"
-      }
-      form.reset()
-    }
-  }
-  form = document.querySelector('[data-js~="todoForm"]');
-
-
-  form.addEventListener(
-      'submit', stopDef, false
-  );
+  form.addEventListener('submit', addItem, false);
 })()
-function deleteCheck(button, indx, arr) {
-      console.log(button)
+
+function addItem(event) {
+  event.preventDefault()
+  input = document.querySelector('[data-js~="todoInput"]').value;
+  if (input === "") {
+    alert("Input is blank. Vader will force choke you for this!")
     }
-//<li class="todo__item">
-//  <input type="checkbox" id="c1" />
-//  <label for="c1">Curse Rebel Scum</label>
-//  <button value=""class='todo__value=""itemRemove'>&#x2717;</button>
-//</li>
+    else {
+    list = document.querySelector('[data-js~="todoItems"]');
+    if (list === null) {
+      list = document.querySelector('[data-js~="newItems"]');
+      list.innerHTML +=" <li class='todo__item'><input type='checkbox' id='" + input + "' /> <label for='" + input + "'>" + input + "</label><button value='" + input + "' class='todo__itemRemove'>&#x2717;</button></li>"
+    }
+    else {
+      list.innerHTML ="<li class='todo__item'><input type='checkbox' id='" + input + "' /> <label for='" + input + "'>" + input + "</label><button value='" + input + "' class='todo__itemRemove'>&#x2717;</button></li>"
+      list.dataset.js = "newItems"
+    }
+  form.reset()
+  
+  this.buttons = document.querySelectorAll('button');
+
+  buttonList = [].slice.call(this.buttons);
+
+  buttonList.forEach(listen);
+  }
+}
+
+function listen(item, indx, arr) {
+  item.addEventListener('click', deleteItem, false);
+}
+
+function deleteItem(event) {
+  this.parentNode.remove();
+} 
